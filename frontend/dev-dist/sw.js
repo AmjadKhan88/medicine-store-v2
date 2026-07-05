95e259cf88fd70a027b1e4bd2d023aa8e719644e
Amjad Ullah <amjadfast87@gmail.com>
2026-07-05 11:26:41 +0500

    fix: update revision number for index.html in service worker

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-d488705a'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.malien63cls"
+    "revision": "0.6c9j0a5vtq"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
