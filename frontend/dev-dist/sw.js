d3968036c466cc2aafed730bc77ff996ae3ecca2
Amjad Ullah <amjadfast87@gmail.com>
2026-07-21 06:07:27 +0500

    missing icons correct

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-d488705a'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.h06b9o4dvrc"
+    "revision": "0.d4kflnfc8io"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
